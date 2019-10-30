73a50268de7bf16735ba2f8a204412f7b52881b6
Ben Budnevich <ben.bj32001@gmail.com>
2019-10-29 23:25:53 -0400

    feat(hooks): update query to search for boards you are a member of

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,10 @@
 export interface QueryResult<QueryDocumentData> {
   data: ({ id: string } & (QueryDocumentData))[];
+  snapshot?: firebase.firestore.QuerySnapshot;
   loading: boolean;
   error?: any;
+}
+
+export enum GroupType {
+  Board = 'board',
 }
