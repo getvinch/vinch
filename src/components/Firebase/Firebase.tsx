06c741d7de3530f7c6d91e61475fa90ec13cc679
Ben Budnevich <ben.bj32001@gmail.com>
2019-10-29 23:23:40 -0400

    feat(firebase): add auth

--- src/components/Firebase/Firebase.tsx
+++ src/components/Firebase/Firebase.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 
 export const FirebaseContext = React.createContext({
   firebase: {},
@@ -27,14 +28,32 @@ interface FirebaseProviderProps {
 
 export function FirebaseProvider(props: FirebaseProviderProps) {
   const [isInitialized, setIsInitialized] = useState(false);
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(() => {
     // Initialize Firebase
     firebase.initializeApp(props.config);
+
+    // Authenticate
+    firebase
+      .auth()
+      .signInAnonymously()
+      .catch(function(error) {});
+
+    // Update logged in state
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        setIsLoggedIn(true);
+      } else {
+        // User is signed out.
+        setIsLoggedIn(false);
+      }
+    });
+
     setIsInitialized(true);
   }, [props.config]);
 
-  if (!isInitialized) {
+  if (!isInitialized || !isLoggedIn) {
     return <>{props.loaderNode ? props.loaderNode : null}</>;
   }
 
