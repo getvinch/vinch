8570d65cd228dd8faebbd805f17e1d9d40417f94
Ben Budnevich <ben.bj32001@gmail.com>
2019-11-11 10:53:37 -0500

    test(boardtile): add story for BoardTile

---
+++ src/components/Boards/components/BoardTile/BoardTile.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import BoardTile from './BoardTile';
+
+export default {
+  title: 'Boards/BoardTile',
+};
+
+export const Default = () => <BoardTile id="1" name="Board A" />;
