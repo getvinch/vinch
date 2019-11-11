508c5b79deabed2a9f4c4f0c46932842d5273161
Ben Budnevich <ben.bj32001@gmail.com>
2019-11-11 12:05:41 -0500

    feat(boards): add ability to create a board

--- src/components/Boards/Boards.stories.tsx
+++ src/components/Boards/Boards.stories.tsx
@@ -25,5 +25,6 @@ export const Default = () => (
         name: 'Board D',
       },
     ]}
+    onAddBoard={() => {}}
   />
 );
---
+++ src/components/Boards/Boards.styles.tsx
@@ -0,0 +1,24 @@
+import { styled } from '@material-ui/core/styles';
+import Fab, { FabProps } from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import { IconProps } from '@material-ui/core/Icon/Icon';
+
+type AddButtonProps = FabProps & {
+  spacing: number;
+};
+
+const AddButton = styled(Fab)({
+  position: 'absolute',
+  bottom: props => props.spacing,
+  right: props => props.spacing,
+}) as React.ComponentType<AddButtonProps>;
+
+type AddButtonIconProps = IconProps & {
+  spacing: number;
+};
+
+const AddButtonIcon = styled(AddIcon)({
+  marginRight: props => props.spacing,
+}) as React.ComponentType<AddButtonIconProps>;
+
+export { AddButton, AddButtonIcon };
