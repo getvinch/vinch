978681887fd1ef2c3bdcf5b3b5c6357a35791fef
Ben Budnevich <ben.bj32001@gmail.com>
2019-11-19 00:52:00 -0500

    test(storybook): add StoryRouter decorator

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,8 @@
 import { configure } from '@storybook/react';
+import { addDecorator } from '@storybook/react';
+import StoryRouter from 'storybook-react-router';
+
+addDecorator(StoryRouter());
 
 // automatically import all files ending in *.stories.{ts,tsx}
 configure(require.context('../src/', true, /\.stories\.tsx?$/), module);
