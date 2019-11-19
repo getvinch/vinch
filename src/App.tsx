9f736678c5da8cac3bcfaee50a2c26f98e45fd84
Ben Budnevich <ben.bj32001@gmail.com>
2019-11-18 20:20:27 -0500

    feat(app): set up top level router and add default route

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
 import Header from './components/Header';
 import { FirebaseProvider } from './components/Firebase';
 import { firebaseConfig } from './constants';
 import './App.css';
 import Boards from './components/Boards';
+import Board from './components/Board';
 
 const App: React.FC = () => {
   return (
     <>
       <CssBaseline />
       <FirebaseProvider config={firebaseConfig}>
-        <Header />
-        <Boards />
+        <Router>
+          <Header />
+          <Route exact path="/" component={Boards} />
+          <Route exact path="/boards/:boardId" component={Board} />
+        </Router>
       </FirebaseProvider>
     </>
   );
