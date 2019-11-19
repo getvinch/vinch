27be8440917a45178a57a7665295c8171c655788
Ben Budnevich <ben.bj32001@gmail.com>
2019-11-19 00:02:23 -0500

    feat(boardtile): hook up route to board

--- src/components/Boards/components/BoardTile/BoardTile.tsx
+++ src/components/Boards/components/BoardTile/BoardTile.tsx
@@ -6,6 +6,7 @@ import CardHeader from '@material-ui/core/CardHeader';
 import Avatar from '@material-ui/core/Avatar';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import Card from '@material-ui/core/Card';
+import * as S from './BoardTile.styles';
 
 interface Props {
   id: string;
@@ -17,14 +18,16 @@ export default function BoardTile(props: Props) {
     <Grid item xs={6} sm={3}>
       <Card>
         <CardActionArea>
-          <CardHeader
-            avatar={
-              <Avatar aria-label="recipe">
-                {props.name ? props.name[0].toUpperCase() : ''}
-              </Avatar>
-            }
-            title={props.name}
-          />
+          <S.MutedLink to={`boards/${props.id}`}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="recipe">
+                  {props.name ? props.name[0].toUpperCase() : ''}
+                </Avatar>
+              }
+              title={props.name}
+            />
+          </S.MutedLink>
         </CardActionArea>
         <CardActions>
           <Button size="small">Edit</Button>
