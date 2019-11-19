55c1536a94cb819e1b41ca7993a714baa6d20572
Ben Budnevich <ben.bj32001@gmail.com>
2019-11-19 01:01:09 -0500

    style(types): reformat types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,12 +1,12 @@
 export interface CollectionQueryResult<QueryDocumentData> {
-  data: ({ id: string } & (QueryDocumentData))[];
+  data: ({ id: string } & QueryDocumentData)[];
   snapshot?: firebase.firestore.QuerySnapshot;
   loading: boolean;
   error?: any;
 }
 
 export interface DocumentQueryResult<DocumentData> {
-  data?: { id: string } & (DocumentData);
+  data?: { id: string } & DocumentData;
   snapshot?: firebase.firestore.DocumentSnapshot;
   loading: boolean;
   error?: any;
