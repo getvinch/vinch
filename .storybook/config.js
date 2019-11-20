3a50405693aa597b9f2e8f5552ccffcc57037d2f
Ben Budnevich <ben.bj32001@gmail.com>
2019-11-19 23:25:45 -0500

    chore(storybook): add storyshots

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,9 @@
 import { configure } from '@storybook/react';
 import { addDecorator } from '@storybook/react';
 import StoryRouter from 'storybook-react-router';
+import requireContext from 'require-context.macro';
 
 addDecorator(StoryRouter());
 
 // automatically import all files ending in *.stories.{ts,tsx}
-configure(require.context('../src/', true, /\.stories\.tsx?$/), module);
+configure(requireContext('../src/', true, /\.stories\.tsx?$/), module);
